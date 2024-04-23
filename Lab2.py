b763f51e5d34dbbb99a63eefb9fbc5843c17313f
Ong Yi <oyyi.23@ichat.sp.edu.sg>
2024-04-23 14:40:02 +0800

    Temperature Measurement Code

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,30 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    x = input()
+    list = x.split(",") #spliting the inputs into list 
+    for i in range (len(list)): #for i in range of length of list eg i=0,1,2,3,4
+       list[i]=float(list[i]) 
+    print(list)  
+    return list
+       
+
+def calc_average_temperature(list):
+    return sum(list)/len(list)
+
+def calc_min_max_temperature(list):
+    return min(list),max(list)
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()        
+    x=get_user_input()                        #if want to run get_user_input before print must make it variable 
+    print(calc_average_temperature(x))  #get_user_input does not need to be called before this as by order of operations get_user_input will run before avg temp
+    print(calc_min_max_temperature(x))
+if __name__ == "__main__":
+ main()
+
+
+
+
