d6bbbfaa8d77f061b87a998a259c833ea576a403
Ong Yi <oyyi.23@ichat.sp.edu.sg>
2024-04-30 13:34:06 +0800

    Added Values

--- bmi.py
+++ bmi.py
@@ -4,8 +4,11 @@ def calculate_bmi(height, weight):
     bmi= weight/(height**2)
     if bmi < 18.5:
         print("You are Under Weight. Your BMI is: " + str(bmi))
+        return -1
     elif bmi  <= 25.0:
         print("You are Acceptable. Your BMI is: " + str(bmi))
+        return 0 
     else:
         print("You are Overweight. Your BMI is: " + str(bmi)) 
-calculate_bmi(weight=107, height=1.73)
+        return 1
+print(calculate_bmi(weight=107, height=1.73))
